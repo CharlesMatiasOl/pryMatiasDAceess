6460fb368d8a5fb72a1037ae1947e9ca283fdb2f
Usuario <Usuario@210318-NUEVA>
2022-10-05 00:17:26 -0300

    Agregar archivos de proyecto.

---
+++ pryMatiasDAceess/frmPrincipal.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace pryMatiasDAceess
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrirTabla = new System.Windows.Forms.Button();
+            this.lblInformacion = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbrirTabla
+            // 
+            this.btnAbrirTabla.Location = new System.Drawing.Point(21, 101);
+            this.btnAbrirTabla.Name = "btnAbrirTabla";
+            this.btnAbrirTabla.Size = new System.Drawing.Size(392, 72);
+            this.btnAbrirTabla.TabIndex = 5;
+            this.btnAbrirTabla.Text = "Abrir Tabla";
+            this.btnAbrirTabla.UseVisualStyleBackColor = true;
+            this.btnAbrirTabla.Click += new System.EventHandler(this.btnAbrirTabla_Click);
+            // 
+            // lblInformacion
+            // 
+            this.lblInformacion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblInformacion.Location = new System.Drawing.Point(21, 247);
+            this.lblInformacion.Name = "lblInformacion";
+            this.lblInformacion.Size = new System.Drawing.Size(392, 124);
+            this.lblInformacion.TabIndex = 4;
+            this.lblInformacion.Text = "Info:";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(21, 23);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(392, 72);
+            this.btnConectar.TabIndex = 3;
+            this.btnConectar.Text = "Conectarme";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 424);
+            this.Controls.Add(this.btnAbrirTabla);
+            this.Controls.Add(this.lblInformacion);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrirTabla;
+        private System.Windows.Forms.Label lblInformacion;
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
+
---
+++ pryMatiasDAceess/frmPrincipal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace pryMatiasDAceess
+{
+    public partial class frmPrincipal : Form
+    {
+        //OleDbConnection conexionBaseDatos;
+        //string cadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0;" +ryMatiasDAceess.git
+            //"Data Source=\\\\servidor\\Co\\VIDEOCLUB.accdb";
+        //OleDbCommand queQuieroTraerDeBD;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexionBaseDatos = new OleDbConnection(cadenaConexion);
+
+                conexionBaseDatos.Open();
+
+                lblInformacion.Text = "Todo ok, CONECTADO - " +
+                    conexionBaseDatos.DataSource.ToString() + "\n";
+            }
+            catch (Exception mensajito)
+            {
+                lblInformacion.Text = mensajito.ToString() + "\n";
+                //  throw;
+            }
+        }
+
+        private void btnAbrirTabla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                queQuieroTraerDeBD = new OleDbCommand();
+
+                queQuieroTraerDeBD.Connection = conexionBaseDatos;
+                queQuieroTraerDeBD.CommandType = CommandType.TableDirect;
+                queQuieroTraerDeBD.CommandText = "Clientes";
+
+                lblInformacion.Text = "Todo ok, tabla " + queQuieroTraerDeBD.CommandText;
+
+                OleDbDataReader objetoLectura = queQuieroTraerDeBD.ExecuteReader();
+
+                while (objetoLectura.Read())
+                {
+                    lblInformacion.Text = lblInformacion.Text + Convert.ToString(objetoLectura["Nombre"]) + "\n";
+                }
+
+                objetoLectura.Close();
+            }
+
+            catch (Exception mensajito)
+            {
+                lblInformacion.Text = mensajito.ToString() + "\n";
+                //throw;
+            }
+        }
+    }
+}
